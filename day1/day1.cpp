81755dbb2ca40f2925f8dc6a40150d2144476091
Benjamin Maurer <maurer.benjamin@gmail.com>
2021-12-16 11:13:41 +0100

    Simplified day1

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -1,41 +1,23 @@
-#include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <numeric>
-#include <string>
 #include <vector>
-
-#include "../include/aoc.hpp"
 
 using vec_input_it = std::vector<int>::const_iterator;
 
-int count_increases(vec_input_it begin, vec_input_it end) {
-    return std::transform_reduce(begin, std::prev(end),
-        std::next(begin), 0,
+int count_increases(vec_input_it begin, vec_input_it end, ptrdiff_t window=1) {
+    return std::transform_reduce(begin, end - window,
+        begin + window, 0,
         std::plus<>(),
-        [](int l, int r) { return (l < r) ? 1 : 0; });
+        std::less<>());
 }
 
 int main() {
 
-    std::vector<int> inp;
-    aoc::read_input(inp);
+    std::vector<int> inp{std::istream_iterator<int>(std::cin), std::istream_iterator<int>()};
 
-    int res1 = count_increases(inp.cbegin(), inp.cend());
-
-    std::cout << "Part 1: " << res1 << '\n';
-
-    // Part 2
-
-    std::vector<int> inp_cpy(inp.size() - 2);
-
-    // Sliding window
-    for (size_t i = 0; i < inp.size() - 2; ++i) {
-        inp_cpy[i] = inp[i] + inp[i + 1] + inp[i + 2];
-    }
-
-    int res2 = count_increases(inp_cpy.cbegin(), inp_cpy.cend());
-
-    std::cout << "Part 2: " << res2 << '\n';
+    std::cout << "Part 1: " << count_increases(inp.cbegin(), inp.cend()) << '\n';
+    std::cout << "Part 2: " << count_increases(inp.cbegin(), inp.cend(), 3) << '\n';
 
     return 0;
 }
